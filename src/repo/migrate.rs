4df2de692383e3fb30388bf132d31a60409b0565
Michael Aaron Murphy <mmstickman@gmail.com>
2018-08-09 16:08:59 -0600

    Migration error handling

--- src/repo/migrate.rs
+++ src/repo/migrate.rs
@@ -1,20 +1,39 @@
 use config::Config;
 use misc::{is_deb, INCLUDE_DDEB, INCLUDE_SRCS};
-use std::io::{self, Error, ErrorKind};
+use std::io::{self, Error};
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::os::unix::fs::MetadataExt;
-use super::generate_release_files;
+use super::{generate_release_files, ReleaseError};
 use walkdir::WalkDir;
 
-pub fn migrate(config: &Config, packages: &[&str], from_component: &str, to_component: &str) -> io::Result<()> {
-    info!("migrating {:?} from {} to {}", packages, from_component, to_component);
+#[derive(Debug, Fail)]
+pub enum MigrationError {
+    #[fail(display = "failed to migrate {:?} to {:?}: {}", src, dst, why)]
+    Move { src: PathBuf, dst: PathBuf, why: Error },
+    #[fail(display = "dist release file generation failed: {}", why)]
+    DistRelease { why: ReleaseError }
+}
 
+impl From<ReleaseError> for MigrationError {
+    fn from(why: ReleaseError) -> Self {
+        MigrationError::DistRelease { why }
+    }
+}
+
+pub fn migrate(config: &Config, packages: &[&str], from_component: &str, to_component: &str) -> Result<(), MigrationError> {
+    info!("migrating {:?} from {} to {}", packages, from_component, to_component);
+    inner_migrate(config, packages, from_component, to_component)?;
+    generate_release_files(&config)?;
+    Ok(())
+}
+
+fn inner_migrate(config: &Config, packages: &[&str], from_component: &str, to_component: &str) -> Result<(), MigrationError> {
     let pool = ["repo/pool/", &config.archive, "/"].concat();
     let src_pool = PathBuf::from([&pool, from_component, "/"].concat());
     let dst_pool = PathBuf::from([&pool, to_component, "/"].concat());
 
-    for package in packages {
+    packages.into_iter().map(|package| {
         let files = WalkDir::new(&src_pool)
             .min_depth(1)
             .max_depth(4)
@@ -28,28 +47,35 @@ pub fn migrate(config: &Config, packages: &[&str], from_component: &str, to_comp
             .flat_map(|e| e.ok())
             .filter(|e| e.depth() == 4);
 
-        for file in files {
-            let src_path = file.path();
-            let dst_path = dst_pool.join(file.path().strip_prefix(&src_pool).unwrap());
+        files.into_iter().map(|file| {
+            let src = file.path();
+            let dst = dst_pool.join(file.path().strip_prefix(&src_pool).unwrap());
 
-            info!("migrating {} to {}", src_path.display(), dst_path.display());
-            if let Some(dst_parent) = dst_path.parent() {
-                if !dst_parent.exists() {
-                    fs::create_dir_all(&dst_parent)?;
-                }
+            migrate_file(&src, &dst).map_err(|why| MigrationError::Move {
+                src: src.to_path_buf(),
+                dst,
+                why
+            })
+        }).collect::<Result<(), MigrationError>>()
+    }).collect()
+}
 
-                let src_metadata = fs::metadata(&src_path)?;
-                let dst_metadata = fs::metadata(&dst_parent)?;
-                if src_metadata.dev() == dst_metadata.dev() {
-                    fs::rename(&src_path, &dst_path)?;
-                } else {
-                    fs::copy(&src_path, &dst_path)?;
-                    fs::remove_file(&src_path)?;
-                }
-            }
+fn migrate_file(src_path: &Path, dst_path: &Path) -> io::Result<()> {
+    if let Some(dst_parent) = dst_path.parent() {
+        info!("migrating {} to {}", src_path.display(), dst_path.display());
+        if !dst_parent.exists() {
+            fs::create_dir_all(&dst_parent)?;
+        }
+
+        let src_metadata = fs::metadata(src_path)?;
+        let dst_metadata = fs::metadata(&dst_parent)?;
+        if src_metadata.dev() == dst_metadata.dev() {
+            fs::rename(src_path, dst_path)?;
+        } else {
+            fs::copy(src_path, dst_path)?;
+            fs::remove_file(src_path)?;
         }
     }
 
-    generate_release_files(&config)
-        .map_err(|why| Error::new(ErrorKind::Other, format!("release file generation: {}", why)))
+    Ok(())
 }
