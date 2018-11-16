517cfcfa9563bbbe48acd900211bb64ea018ff0d
Michael Aaron Murphy <mmstickman@gmail.com>
2018-11-16 16:20:20 -0700

    Fix asset build errors

--- src/repo/build/rsync.rs
+++ src/repo/build/rsync.rs
@@ -5,12 +5,12 @@ use std::{io, fs};
 pub fn rsync(src: &Path, dst: &Path) -> io::Result<()> {
     info!("rsyncing {} to {}", src.display(), dst.display());
 
-    if src.is_dir() {
+    if src.is_dir() && ! dst.exists() {
         fs::create_dir_all(dst).map_err(|why| io::Error::new(
             io::ErrorKind::Other,
             format!("failed to create destination directory at {:?} for rsync: {}", dst, why)
         ))?;
     }
 
-    Command::new("rsync").arg("-avz").arg(src).arg(dst).run()
+    Command::new("rsync").args(&["--ignore-existing", "-avz"]).arg(src).arg(dst).run()
 }
